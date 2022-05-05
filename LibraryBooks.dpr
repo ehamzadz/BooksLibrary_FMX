980b649e6eb5beb208a048177653be3167d9aee4
eHamzaDZ <ehamzadz@pm.me>
2022-05-05 03:34:54 +0100

    add loading design

--- LibraryBooks.dpr
+++ LibraryBooks.dpr
@@ -3,12 +3,16 @@ program LibraryBooks;
 uses
   System.StartUpCopy,
   FMX.Forms,
-  loading in 'loading.pas' {Form2};
+  loading in 'loading.pas' {frmLoading},
+  load in 'load.pas' {frmLoad},
+  loadingIcon in 'loadingIcon.pas' {Form1};
 
 {$R *.res}
 
 begin
   Application.Initialize;
-  Application.CreateForm(TForm2, Form2);
+  Application.CreateForm(TfrmLoad, frmLoad);
+  Application.CreateForm(TfrmLoading, frmLoading);
+  Application.CreateForm(TForm1, Form1);
   Application.Run;
 end.
---
+++ __history/LibraryBooks.dpr.~1~
@@ -0,0 +1,14 @@
+program LibraryBooks;
+
+uses
+  System.StartUpCopy,
+  FMX.Forms,
+  loading in 'loading.pas' {Form2};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TForm2, Form2);
+  Application.Run;
+end.
